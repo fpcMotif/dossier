39c73252a083bfb7ad9ee8193b13e1168bd1d49f
Steven Frank <stevenf@panic.com>
2014-09-02 22:21:39 +0000

    add config.php with private, public, and read-only access modes

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+	
+// ACCESS_CONTROL
+//
+// public = everyone can see and edit everything
+// read-only = everyone can see everything, authenticated users can edit everything
+// private = only authenticated users can see or edit anything	
+
+define('ACCESS_CONTROL', 'private');
